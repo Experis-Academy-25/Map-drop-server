3465f1309bf967f0d9e104a64ed9298ed1eb4eda
Wilmer Winkler <wilmer.winkler@se.experis.com>
2025-04-01 16:39:59 +0200

    Endpoint to update a game

--- src/main/java/com/mapdrop/controllers/GameController.java
+++ src/main/java/com/mapdrop/controllers/GameController.java
@@ -32,6 +32,8 @@ public class GameController {
     private GameResponse gameResponse = new GameResponse();
     private GameListResponse gameListResponse = new GameListResponse();
 
+    private int gameCost = 30;
+
     @PostMapping("{user_id}")
     public ResponseEntity<Response<?>> createGame(Authentication authentication, @RequestBody Game game, @PathVariable(name = "user_id") int userId) {
         User playingUser = this.userRepository.findById(userId).orElse(null);
@@ -48,10 +50,45 @@ public class GameController {
         game.setUser(playingUser);
         game.setCreatedAt(LocalDateTime.now());
         game.setUpdatedAt(LocalDateTime.now());
-        playingUser.setTotalPoints(playingUser.getTotalPoints() + game.getPoints());
+        playingUser.setTotalPoints(playingUser.getTotalPoints() - gameCost);
 
         try {
             gameResponse.set(this.gameRepository.save(game));
+        } catch (Exception e) {
+            errorResponse.set("Bad request");
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(gameResponse, HttpStatus.CREATED);
+    }
+    @PutMapping("{user_id}/{game_id}")
+    public ResponseEntity<Response<?>> updateGame(Authentication authentication, @PathVariable(name = "game_id") int gameID, @RequestBody Game updatedGame, @PathVariable(name = "user_id") int userId) {
+        User playingUser = this.userRepository.findById(userId).orElse(null);
+        if (playingUser == null) {
+            errorResponse.set("not found");
+            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
+
+        Game gameToUpdate = this.gameRepository.findById(gameID).orElse(null);
+        if (gameToUpdate == null) {
+            errorResponse.set("not found");
+            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
+
+        if (!playingUser.getUsername().equals(authentication.getName())) {
+            errorResponse.set("forbidden request");
+            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+        }
+        gameToUpdate.setLatitude_guess(updatedGame.getLatitude_guess());
+        gameToUpdate.setLongitude_guess(updatedGame.getLongitude_guess());
+        gameToUpdate.setLatitude_real(updatedGame.getLatitude_real());
+        gameToUpdate.setLongitude_real(updatedGame.getLongitude_real());
+        gameToUpdate.setPoints(updatedGame.getPoints());
+        gameToUpdate.setLocation(updatedGame.getLocation());
+        gameToUpdate.setUpdatedAt(LocalDateTime.now());
+        playingUser.setTotalPoints(playingUser.getTotalPoints() + updatedGame.getPoints() + gameCost);
+
+        try {
+            gameResponse.set(this.gameRepository.save(gameToUpdate));
         } catch (Exception e) {
             errorResponse.set("Bad request");
             return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
